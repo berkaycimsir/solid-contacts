53f5a0f588ffe0c61633770081c95eb0dc451a0b
berkaycimsir <berkaycimsir2003@hotmail.com>
2022-09-17 00:57:33 +0300

    changed loading skeletons design

--- src/components/Contact/ContactSkeletonItem.tsx
+++ src/components/Contact/ContactSkeletonItem.tsx
@@ -1,4 +1,10 @@
-import { Box, hope, SkeletonCircle, SkeletonText } from '@hope-ui/solid';
+import {
+  Box,
+  hope,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+} from '@hope-ui/solid';
 import { Component } from 'solid-js';
 
 const StyledCard = hope(Box, {
@@ -6,15 +12,44 @@ const StyledCard = hope(Box, {
     borderWidth: 1,
     borderColor: '$neutral6',
     borderRadius: '$sm',
-    padding: '$8',
+    padding: '$6',
+  },
+});
+
+const StyledCardHeader = hope(Box, {
+  baseStyle: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+const StyledCardBottom = hope(Box, {
+  baseStyle: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    mt: '$6',
   },
 });
 
 const ContactSkeletonItem: Component = () => {
   return (
     <StyledCard>
-      <SkeletonCircle size="$10" />
-      <SkeletonText mt="$4" noOfLines={4} spacing="$4" />
+      <StyledCardHeader>
+        <SkeletonCircle w="$12" h="$10" />
+
+        <Box w="$full" ml="$4">
+          <SkeletonText w="$28" noOfLines={1} />
+          <SkeletonText w="$48" mt="$4" noOfLines={1} />
+        </Box>
+      </StyledCardHeader>
+      <SkeletonText mt="$6" noOfLines={2} spacing="$4" />
+
+      <StyledCardBottom>
+        <Skeleton w="$24" h="$10" />
+
+        <Skeleton w="$10" h="$10" />
+      </StyledCardBottom>
     </StyledCard>
   );
 };
